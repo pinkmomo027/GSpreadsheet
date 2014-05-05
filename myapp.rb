bb969da0964ce2e82f11931bc78ddc647aa74cdb
Liz Liz <lli@oversee.net>
2014-05-05 13:13:17 -0700

    Post form to ruby and ender success

--- myapp.rb
+++ myapp.rb
@@ -6,7 +6,16 @@ require 'sass'
 require 'spreadsheet'
 
 get '/' do
-
   spreadsheet = Spreadsheet.new
   haml :index, :format => :html5, :locals => {:columns => spreadsheet.columns}
+end
+
+post '/' do
+	p " = " * 20
+	p params
+	redirect '/thankyou'
+end
+
+get '/thankyou' do
+	haml :thankyou, :format => :html5
 end
