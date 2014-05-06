989e368bafc09a416bbbb941d2f18e3a589f0756
Liz Liz <lli@oversee.net>
2014-05-06 12:48:22 -0700

    Add post to spreadsheet

--- myapp.rb
+++ myapp.rb
@@ -2,16 +2,17 @@
 require 'sinatra'
 require 'Haml'
 require 'sass'
-
 require 'spreadsheet'
+
+set :spreadsheet, Spreadsheet.new
 
 get '/' do
   spreadsheet = Spreadsheet.new
-  haml :index, :format => :html5, :locals => {:columns => spreadsheet.columns}
+  columns = spreadsheet.columns
+  haml :index, :format => :html5, :locals => {:columns => columns}
 end
 
 post '/' do
-	p " = " * 20
-	p params
+	settings.spreadsheet.add_row(params)
 	haml :thankyou, :format => :html5
 end
--- spreadsheet.rb
+++ spreadsheet.rb
@@ -7,11 +7,21 @@ class Spreadsheet
 
 	def initialize
 		@session = GoogleDrive.login("lizligithub@gmail.com", "githubaccount")
+    @file = @session.spreadsheet_by_key('1M_CZgLnuLUzsfdhNahPY6WoH8NQklXyj12Ll_qpucYQ')
 	end
 
 	def columns
-		file = @session.spreadsheet_by_key('1M_CZgLnuLUzsfdhNahPY6WoH8NQklXyj12Ll_qpucYQ')
-		rows = file.worksheets[1].rows.map(&:first)
-		rows
+		@file.worksheets[1].rows.map(&:first)
 	end
+
+  def add_row(params)
+    worksheet = @file.worksheets[0]
+    index = worksheet.num_rows + 1
+    worksheet[index, 1] = params["first_name"]
+    worksheet[index, 2] = params["last_name"]
+    worksheet[index, 3] = params['date_of_birth']
+    worksheet[index, 4] = params['birthday_display']
+    worksheet.save()
+  end
+
 end
